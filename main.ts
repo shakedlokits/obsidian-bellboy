babc842fc85795082452882691663c3c79a30059
Shaked Lokits <shaked.lokits@gmail.com>
2022-05-20 21:49:23 +0300

    Extracted the file update method.

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
-import { Plugin, TFile } from 'obsidian';
+import { CachedMetadata, Plugin, TFile } from 'obsidian';
 import kebabCase from 'lodash/kebabCase';
 import trim from 'lodash/trim';
 
 export default class Bellboy extends Plugin {
+	async updateFileName(file: TFile, data: string, cache: CachedMetadata) {
+		const [{ heading: title }] = cache?.headings ?? [{ heading: file.basename }];
+		const { icon } = cache?.frontmatter;
+
+		const fileName = `${kebabCase(title)}${icon ? '-' + trim(icon) : ''}.md`;
+		if (file.basename !== fileName) {
+			await this.app.fileManager.renameFile(file, fileName);
+		}
+	}
+
 	async onload() {
 		this.registerEvent(
 			this.app.metadataCache.on('changed', async (file, data, cache) => {
-				if (file instanceof TFile) {
-					const [{ heading: title }] = cache?.headings ?? [{ heading: file.basename }];
-					const { icon } = cache?.frontmatter;
-
-					const fileName = `${kebabCase(title)}${icon ? '-' + trim(icon) : ''}.md`;
-					if (file.basename !== fileName) {
-						await this.app.fileManager.renameFile(file, fileName);
-					}
-				}
+				await this.updateFileName(file, data, cache);
 			})
 		);
 	}
