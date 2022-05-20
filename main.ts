c72a44434b38a6d25ed9e1f77763660e90889d4b
Shaked Lokits <shaked.lokits@gmail.com>
2022-05-20 20:57:15 +0300

    Review: Made changes to address initial review.

--- main.ts
+++ main.ts
@@ -1,40 +1,21 @@
-import { Plugin, parseYaml, TFile } from "obsidian";
-import { kebabCase, trim } from "lodash";
+import { Plugin, TFile } from 'obsidian';
+import kebabCase from 'lodash/kebabCase';
+import trim from 'lodash/trim';
+
 export default class Bellboy extends Plugin {
-  async parseFrontMatter(file: TFile) {
-    const contents = await this.app.vault.cachedRead(file);
-    const { header } = contents.match(/---\n(?<header>.*)\n---/s).groups;
-    if (!header) return {};
+	async onload() {
+		this.registerEvent(
+			this.app.metadataCache.on('changed', async (file, data, cache) => {
+				if (file instanceof TFile) {
+					const [{ heading: title }] = cache?.headings ?? [{ heading: file.basename }];
+					const { icon } = cache?.frontmatter;
 
-    const frontMatter = parseYaml(header);
-    return frontMatter;
-  }
-
-  async getSanitizedFileName(file: TFile) {
-    const contents = await this.app.vault.cachedRead(file);
-
-    const { title } = contents.match(/\s*#+ (?<title>.*)$/m).groups;
-    if (!title) return file.name;
-
-    return kebabCase(title);
-  }
-
-  async constructFileName(file: TFile) {
-    const sanitizedFileName = await this.getSanitizedFileName(file);
-    const { icon } = await this.parseFrontMatter(file);
-
-    const fileName = `${sanitizedFileName}${icon ? "-" + trim(icon) : ""}.md`;
-    return fileName;
-  }
-
-  async onload() {
-    this.registerEvent(
-      this.app.vault.on("modify", async (file) => {
-        if (file instanceof TFile) {
-          const fileName = await this.constructFileName(file);
-          await this.app.fileManager.renameFile(file, fileName);
-        }
-      })
-    );
-  }
+					const fileName = `${kebabCase(title)}${icon ? '-' + trim(icon) : ''}.md`;
+					if (file.basename !== fileName) {
+						await this.app.fileManager.renameFile(file, fileName);
+					}
+				}
+			})
+		);
+	}
 }
